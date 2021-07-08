f8c745839a77a036e3f040533d8c25baadf01dcf
AlexandreZelante <alexandre.zelante@outlook.com>
2021-07-07 22:46:09 -0300

    adaptive threshold test code

--- index.py
+++ index.py
@@ -15,40 +15,97 @@ import cv2
 INPUT_DIR = "./input/"
 OUTPUT_DIR = "./output/"
 
+imageHeight = 0
+imageWidth = 0
+
 def convertToGrayScale(imageArray):
   grayImage = []
-
-  imageHeight = len(imageArray)
-  imageWidth = len(imageArray[0])
-
+  
   for i in range(imageHeight):
       for j in range(imageWidth):
           grayImage.append(int(imageArray[i][j][0]*0.2126 + imageArray[i][j][1]*0.7152 + imageArray[i][j][2] * 0.0722)) 
 
   return grayImage
 
+def applyAdaptiveThreshold(imageArray):
+  adaptiveThresholdImage = []
+
+  th3 = cv2.adaptiveThreshold(imageArray,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY,11,2)
+
+  return th3
+
+def applyAdaptiveThresholdTest():
+  # input_img = cv2.imread('./input/rg.jpg')
+
+  # h = input_img.shape[0]
+  # w = input_img.shape[1]
+
+  # S = w/8
+  # s2 = S/2
+  # T = 15.0
+
+  # # print(h)
+
+  # #integral img
+  # int_img = numpy.zeros_like(input_img, dtype=numpy.uint32)
+  # print(int_img)
+  # for col in range(w):
+  #   for row in range(h):
+  #     # print('row')
+  #     int_img[row,col] = input_img[0:row,0:col].sum()
+  #output img
+  # out_img = numpy.zeros_like(input_img)    
+
+  # for col in range(w):
+  #     for row in range(h):
+  #         #SxS region
+  #         y0 = max(row-s2, 0)
+  #         y1 = min(row+s2, h-1)
+  #         x0 = max(col-s2, 0)
+  #         x1 = min(col+s2, w-1)
+
+  #         count = (y1-y0)*(x1-x0)
+
+  #         sum_ = int_img[y1, x1]-int_img[y0, x1]-int_img[y1, x0]+int_img[y0, x0]
+
+  #         if input_img[row, col]*count < sum_*(100.-T)/100.:
+  #             out_img[row,col] = 0
+  #         else:
+  #             out_img[row,col] = 255
+
+  # return out_img
+
 
 def pipeline():
   for filename in os.listdir(INPUT_DIR):
-    print(filename)
-    im = Image.open(INPUT_DIR + filename) #1152 x 864
+    if filename.endswith(".jpg") or filename.endswith(".png"):
+      print(filename)
+      im = Image.open(INPUT_DIR + filename) #1152 x 864
 
-    imageArray = numpy.asarray(im)
+      imageArray = numpy.asarray(im)
 
-    imageHeight = len(imageArray)
-    imageWidth = len(imageArray[0])
-    print("Numero de linhas: "+ str(len(imageArray))) #Linha 0, coluna 0
-    print("Numero de colunas: "+ str(len(imageArray[0]))) #Linha 0, coluna 0
-    
-    # Step 1: Convert to Gray Scale
-    grayImage = convertToGrayScale(imageArray)
+      global imageHeight
+      imageHeight = len(imageArray)
 
-    # Step 2: Adaptive Threshold
-    
+      global imageWidth
+      imageWidth = len(imageArray[0])
+      print("Numero de linhas: "+ str(len(imageArray)))
+      print("Numero de colunas: "+ str(len(imageArray[0])))
+      
+      # Step 1: Convert to Gray Scale
+      grayImage = convertToGrayScale(imageArray)
+      cv2.imwrite(OUTPUT_DIR + "grayImage_" + filename, numpy.array(grayImage).reshape(imageHeight,imageWidth))
+      print("Imagem em escala de cinza gerada")
 
-    # Save image
-    cv2.imwrite(OUTPUT_DIR + "final_" + filename, numpy.array(grayImage).reshape(imageHeight,imageWidth))
+      # Step 2: Adaptive Threshold
+      adaptiveThresholdImage = applyAdaptiveThresholdTest(imageArray)
+      cv2.imwrite(OUTPUT_DIR + "adaptiveThresholdImage_" + filename, numpy.array(adaptiveThresholdImage).reshape(imageHeight,imageWidth))
+      print("Imagem com o Threshold aplicado gerado")
+      
+
+      # Save image
+      cv2.imwrite(OUTPUT_DIR + "final_" + filename, numpy.array(grayImage).reshape(imageHeight,imageWidth))
 
 
 pipeline()
-
+# print(applyAdaptiveThresholdTest())
--- test.py
+++ test.py
@@ -1,16 +1,26 @@
 import cv2
-import numpy as np
-img1 = cv2.imread('./input/rg.jpg')
-row,col,ch = img1.shape
-g = [ ]  #the list in which we will stuff single grayscale pixel value inplace of 3 RBG values
-#this function converts each RGB pixel value into single Grayscale pixel value and appends that value to list 'g'
-def rgb2gray(Img):
-  global g
-  row,col,CHANNEL = Img.shape
-  for i in range(row):
-    for j in range(col):
-      a = (Img[i,j,0]*0.07 + Img[i,j,1]*0.72 + Img[i,j,2] *0.21) #the algorithm i used id , G =  B*0.07 + G*0.72 + R* 0.21
-      g.append(a)
-rgb2gray(img1)  #convert the img1 into grayscale
-gr = np.array(g)  #convert the list 'g' containing grayscale pixel values into numpy array
-cv2.imwrite("test1.png" , gr.reshape(row,col)) #save the image file as test1.jpg
+import numpy
+def convertToGrayScale(imageArray):
+    grayImage = []
+    imageHeight = len(imageArray)
+    imageWidth = len(imageArray[0])
+
+    for i in range(imageHeight):
+        for j in range(imageWidth):
+            grayImage.append(int(imageArray[i][j][0]*0.2126 + imageArray[i][j][1]*0.7152 + imageArray[i][j][2] * 0.0722)) 
+    
+    return grayImage
+
+def applyAdaptiveThreshold():
+    adaptiveThresholdImage = []
+    img = cv2.imread('./input/rg.jpg',0)
+    imageArray = numpy.asarray(img)
+    img = cv.medianBlur(imageArray,5)
+    
+    # grayImage = convertToGrayScale(imageArray)
+
+    th3 = cv2.adaptiveThreshold(img,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY,11,2)
+
+    return th3
+
+applyAdaptiveThreshold()
